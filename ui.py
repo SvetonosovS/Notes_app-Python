a3eefaf76638c76ed558f9e4c3c21375e6f9c07f
SvetonosovS <tris23@bk.ru>
2023-03-29 21:44:14 +0300

    creating intermediate work

---
+++ ui.py
@@ -0,0 +1,32 @@
+import Note
+
+def create_note(number):
+    title = check_len_text_input(
+        input('Введите Название заметки: '), number)
+    body = check_len_text_input(
+        input('Введите Описание заметки: '), number)
+    return Note.Note(title=title, body=body)
+
+
+def menu():
+    print("""\nВыберите действие:\n
+        1 - Показать все записи
+        2 - Создание  заметки
+        3 - Удаление заметки
+        4 - Редактирование заметки
+        5 - Выборка заметок по дате
+        6 - Показать заметку по id
+        0 - Выход\n
+        Введите номер функции: """)
+
+
+def check_len_text_input(text, n):
+    while len(text) <= n:
+        print(f'Текст должен быть больше {n} символа\n')
+        text = input('Введите тескт: ')
+    else:
+        return text
+
+
+def exiting_app():
+    print("Приложение не активно")
